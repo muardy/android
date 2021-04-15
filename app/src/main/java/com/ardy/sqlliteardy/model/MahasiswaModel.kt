9f0a93e8f5089087a1c142ecb641af00a14a55b8
muardy <muardy.irawan@gmail.com>
2021-04-15 12:26:15 +0700

    awalan projek

---
+++ app/src/main/java/com/ardy/sqlliteardy/model/MahasiswaModel.kt
@@ -0,0 +1,12 @@
+package com.ardy.sqlliteardy.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class MahasiswaModel(
+    var id: Int = 0,
+    var name: String? = null,
+    var nim: String? = null
+): Parcelable
